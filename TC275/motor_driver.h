6b58031521bcaa8f7bdf779f0e0ad7beedd127ca
kimjunhyeok <83cpad83@gmail.com>
2025-05-20 17:08:55 +0900

    feat: dc모터 드라이버 구현 (#12)

---
+++ TC275/motor_driver.h
@@ -0,0 +1,63 @@
+﻿/**********************************************************************************************************************
+ * \file driver_motor.h
+ * \copyright Copyright (C) Infineon Technologies AG 2019
+ *********************************************************************************************************************/
+#ifndef DRIVER_MOTOR_H_
+#define DRIVER_MOTOR_H_
+/*********************************************************************************************************************/
+/*-----------------------------------------------------Includes------------------------------------------------------*/
+/*********************************************************************************************************************/
+#include "Ifx_Types.h"
+#include "IfxPort_PinMap.h"
+#include "IfxGtm_Tom_Pwm.h"
+/*********************************************************************************************************************/
+/*------------------------------------------------------Macros-------------------------------------------------------*/
+/*********************************************************************************************************************/
+//각 모터의 en과 연결되는 pwm
+#define MOTOR_FL IfxGtm_TOM0_1_TOUT103_P10_1_OUT
+#define MOTOR_FR IfxGtm_TOM0_2_TOUT104_P10_2_OUT
+#define MOTOR_RL IfxGtm_TOM0_3_TOUT105_P10_3_OUT
+#define MOTOR_RR IfxGtm_TOM0_4_TOUT30_P33_8_OUT
+
+#define MOTOR_COUNT 4
+
+#define MOTOR_PERIOD 5000
+#define MOTOR_DUTY_MIN 4000
+#define MOTOR_DUTY_DEFAULT 0
+#define MOTOR_DUTY_UNIT ((MOTOR_PERIOD - MOTOR_DUTY_MIN) / 100)
+
+//방향 설정용 디지털output 핀, 모터당 2개 - IN1, IN2
+#define IN1_FL IfxPort_P14_0
+#define IN2_FL IfxPort_P14_1
+
+#define IN1_FR IfxPort_P15_6
+#define IN2_FR IfxPort_P00_0
+
+#define IN1_RL IfxPort_P00_8
+#define IN2_RL IfxPort_P00_1
+
+#define IN1_RR IfxPort_P00_9
+#define IN2_RR IfxPort_P00_2
+
+/*********************************************************************************************************************/
+/*-------------------------------------------------Data Structures---------------------------------------------------*/
+/*********************************************************************************************************************/
+typedef enum motor_index{
+    INDEX_FL = 0,
+    INDEX_FR = 1,
+    INDEX_RL = 2,
+    INDEX_RR = 3
+}motor_index_t;
+
+/*********************************************************************************************************************/
+/*------------------------------------------------Function Prototypes------------------------------------------------*/
+/*********************************************************************************************************************/
+void initMotor(void);
+void init_pwms(IfxGtm_Tom_Pwm_Driver* drivers[] , IfxGtm_Tom_Pwm_Config* configs[], int pwm_count);
+void motor_run_forward(motor_index_t motor_index);
+void motor_run_backward(motor_index_t motor_index);
+void motor_stop(motor_index_t motor_index);
+void set_motor_power(motor_index_t motor_index, uint32 dutyPercent);
+void _setDutyCycle(IfxGtm_Tom_Pwm_Driver* driver , IfxGtm_Tom_Pwm_Config* config, uint32 _dutyCycle);
+
+#endif /* DRIVER_MOTOR_H_ */
