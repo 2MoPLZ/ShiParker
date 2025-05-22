3ce3914dd8aeaae74780c909879d18891fab6686
오동걸 <odongg@naver.com>
2025-05-22 18:41:33 +0900

    feat: Error handling 및 장애물 탐지 비상정지 기능 구현

--- TC275/shiparker_App.c
+++ TC275/shiparker_App.c
@@ -1,13 +1,16 @@
 #include "shiparker_App.h"
 
-boolean g_isAppRunning;
-static struct ParkingSystemPacket carStatusPacket = {};
-static CAR_STATUS_TYPE carStatus;
-static CAR_COMMAND_TYPE carCommand;
-static struct Position currentPosition;
-static struct Position targetPosition;
+#include "ultrasonic_Driver.h"
 
-ISR2(AppTimerISR){
+boolean                           g_isAppRunning;
+static struct ParkingSystemPacket carStatusPacket = {};
+static CAR_STATUS_TYPE            carStatus;
+static CAR_COMMAND_TYPE           carCommand;
+static struct Position            currentPosition;
+static struct Position            targetPosition;
+
+ISR2(AppTimerISR)
+{
     osEE_tc_stm_set_sr1_next_match(50000U);
     IncrementCounter(AppCounter);
 }
@@ -20,24 +23,25 @@ void startShiParkerApp(void)
         CancelAlarm(PacketSendAlarm);
         CancelAlarm(AppAlarm);
     }
-    else{
+    else
+    {
         printfSerial("start ShiParker...\n");
     }
-    g_isAppRunning = TRUE;
-    carStatus             = CAR_STATUS_READY;
+    g_isAppRunning    = TRUE;
+    carStatus         = CAR_STATUS_READY;
     currentPosition.x = 0;
     currentPosition.y = 0;
     targetPosition.x  = -1; // NULL 값을 따로 정의해줘야함
     targetPosition.y  = -1; // NULL 값을 따로 정의해줘야함
-    carCommand            = CAR_COMMAND_STOP;
+    carCommand        = CAR_COMMAND_STOP;
     SetRelAlarm(AppAlarm, 1, 10);
     SetRelAlarm(PacketSendAlarm, 2, 50);
 }
 
-TASK (ShiParkerAppTask)
+TASK(ShiParkerAppTask)
 {
     printfSerial("app");
-    if(g_isAppRunning==FALSE)
+    if (g_isAppRunning == FALSE)
         TerminateTask();
     updateStatus(&g_RecievedParkingSystemPacket);
     switch (carStatus)
@@ -54,6 +58,7 @@ TASK (ShiParkerAppTask)
             carStatus = CAR_STATUS_RUNNING;
             CancelAlarm(PacketSendAlarm);
             SetRelAlarm(PacketSendAlarm, 0, 10);
+            SetRelAlarm(AvoidObstacleAlarm, 0, 2); // 100ms
             break;
         case CAR_COMMAND_STOP:
             // READY일때 일시정지 -> 변화없음
@@ -75,6 +80,7 @@ TASK (ShiParkerAppTask)
         case CAR_COMMAND_STOP:
             // RUNNING일때 일시정지 -> 일시정지
             CancelAlarm(PacketSendAlarm);
+            CancelAlarm(AvoidObstacleAlarm);
             SetRelAlarm(PacketSendAlarm, 0, 50);
             break;
         default:
@@ -92,7 +98,8 @@ TASK (ShiParkerAppTask)
             // STOP일때 시작 -> 주차 재개
             carStatus = CAR_STATUS_RUNNING;
             CancelAlarm(PacketSendAlarm);
-            SetRelAlarm(PacketSendAlarm,0,10);
+            SetRelAlarm(PacketSendAlarm, 0, 10);
+            SetRelAlarm(AvoidObstacleAlarm, 0, 2);
             break;
         case CAR_COMMAND_STOP:
             // STOP일때 일시정지 -> 변화없음
@@ -102,15 +109,17 @@ TASK (ShiParkerAppTask)
         }
         break;
     case CAR_STATUS_TERMINATED:
-        //앱을 종료하고 차량 제어권을 사용자에게 넘긴다
+        // 앱을 종료하고 차량 제어권을 사용자에게 넘긴다
         printfSerial("Terminate ShiParker...\n");
         ActivateTask(PacketSendTask);
         g_isAppRunning = FALSE;
+        CancelAlarm(AvoidObstacleAlarm);
         CancelAlarm(PacketSendAlarm);
         CancelAlarm(AppAlarm);
         TerminateTask();
         break;
-    case CAR_STATUS_ERROR:
+    case CAR_STATUS_ERROR_OBSTACLE:
+    case CAR_STATUS_ERROR_BAD_CONNECTION:
         switch (carCommand)
         {
         case CAR_COMMAND_FORCESTOP:
@@ -121,7 +130,8 @@ TASK (ShiParkerAppTask)
             // ERROR일때 시작 -> 주차 재개
             carStatus = CAR_STATUS_RUNNING;
             CancelAlarm(PacketSendAlarm);
-            SetRelAlarm(PacketSendAlarm,0,50);//2500ms
+            SetRelAlarm(PacketSendAlarm, 0, 50); // 2500ms
+            SetRelAlarm(AvoidObstacleAlarm, 0, 2);
             break;
         case CAR_COMMAND_STOP:
             // ERROR일때 일시정지 -> 변화없음
@@ -138,51 +148,73 @@ TASK (ShiParkerAppTask)
     }
 }
 
-TASK(PacketSendTask){
+TASK(AvoidObstacleTask)
+{
+    if (g_isAppRunning == FALSE)
+        TerminateTask();
+    if (getUltrasonic(&g_Ultrasonic_FRONT) < FRONT_OBSTACLE_THRESHOLD)
+    {
+        handleError(ERROR_CODE_OBSTACLE);
+    }
+}
+
+// TASK(WallFollowTask){
+//     //RUNNING 중이고, (목표위치-현재위치)>허용오차 이고, 긴급정지플래그 ==
+//     // false 인 동안 double allowableXDifference = 3.00; double
+//     // allowableYDifference = 3.00; if()
+// }
+
+TASK(PacketSendTask)
+{
     printfSerial("sendpacket");
     makePacket(&carStatusPacket);
     sendPacket(&carStatusPacket);
 }
 
-void makePacket(struct ParkingSystemPacket* dst){
-    dst->start_byte = 0xAA;
-    dst->car_status = carStatus;
-    dst->car_command = carCommand;
+void makePacket(struct ParkingSystemPacket *dst)
+{
+    dst->start_byte             = 0xAA;
+    dst->car_status             = carStatus;
+    dst->car_command            = carCommand;
     dst->car_current_position.x = currentPosition.x;
     dst->car_current_position.y = currentPosition.y;
-    dst->car_target_position.x = targetPosition.x;
-    dst->car_target_position.y = targetPosition.y;
+    dst->car_target_position.x  = targetPosition.x;
+    dst->car_target_position.y  = targetPosition.y;
 }
-void updateStatus(const struct ParkingSystemPacket* packet)
+void updateStatus(const struct ParkingSystemPacket *packet)
 {
-    carStatus=packet->car_status;
-    carCommand=packet->car_command;
-    currentPosition.x=packet->car_current_position.x;
-    currentPosition.y=packet->car_current_position.y;
-    targetPosition.x=packet->car_target_position.x;
-    targetPosition.y=packet->car_target_position.y;
+    carStatus         = packet->car_status;
+    carCommand        = packet->car_command;
+    currentPosition.x = packet->car_current_position.x;
+    currentPosition.y = packet->car_current_position.y;
+    targetPosition.x  = packet->car_target_position.x;
+    targetPosition.y  = packet->car_target_position.y;
 }
 
-void handleError(ERROR_CODE_TYPE errorCode) {
-    carStatus = CAR_STATUS_ERROR;
-    if (errorCode < ERROR_CODE_MAX) {
-        printfSerial("ERROR: %s (%d)\n",errorMessages[errorCode],errorCode);
+void handleError(ERROR_CODE_TYPE errorCode)
+{
+    CancelAlarm(AvoidObstacleAlarm);
+    if (errorCode < ERROR_CODE_MAX)
+    {
+        printfSerial("ERROR: %s (%d)\n", errorMessages[errorCode], errorCode);
         switch (errorCode)
         {
         case ERROR_CODE_USER_CONTROL:
-            //ERROR HANDLING CODE
+            carStatus = CAR_STATUS_ERROR_HARDWARE;
             break;
         case ERROR_CODE_OBSTACLE:
-            //ERROR HANDLING CODE
+            carStatus = CAR_STATUS_ERROR_OBSTACLE;
             break;
         case ERROR_CODE_CONNECTION_LOST:
-            //ERROR HANDLING CODE
+            carStatus = CAR_STATUS_ERROR_BAD_CONNECTION;
             break;
         default:
             break;
         }
     }
-    else {
+    else
+    {
         printfSerial("ERROR: Unknown error code (%d).\n", errorCode);
     }
+    ActivateTask(PacketSendTask);
 }
